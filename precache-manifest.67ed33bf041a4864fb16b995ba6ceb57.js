b84f2e651d1c78c91f97e79ff518b23bfa15b551
Shinu <spushpan@expediagroup.com>
2020-03-22 00:45:35 -0700

    Updates

---
+++ precache-manifest.67ed33bf041a4864fb16b995ba6ceb57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a91afdf680363307f7ce27d79398bf",
+    "url": "/shinup/moster-inc/index.html"
+  },
+  {
+    "revision": "46d906ec6d072df2dc67",
+    "url": "/shinup/moster-inc/static/css/main.2239c45a.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/shinup/moster-inc/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/shinup/moster-inc/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46d906ec6d072df2dc67",
+    "url": "/shinup/moster-inc/static/js/main.a74cdca9.chunk.js"
+  },
+  {
+    "revision": "c47f1f5e401b71085b6f",
+    "url": "/shinup/moster-inc/static/js/runtime-main.5c97aba3.js"
+  }
+]);
